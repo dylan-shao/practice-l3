ab84a312d16562a7fc55a37f6b360c14df375e34
shao <yangshao2014@gmail.com>
2016-04-03 12:52:44 -0400

    added Pagination by angular-ui

--- public/app.js
+++ public/app.js
@@ -5,12 +5,17 @@
                 $http.get('/interview')
                     .then(function(data) {
                         $scope.interviews = data.data;
-                    })
+                    });
                 $scope.showQuestions = function(interview) {
+                    $scope.questions = interview.questions;
                     $uibModal.open({
                         templateUrl: '/questions.html',
                         controller: function($scope) {
                             $scope.questions = interview.questions;
+                            $scope.totalItems = interview.questions.length;
+                            $scope.currentPage = 1;
+                            $scope.maxSize = 5;
+                            $scope.itemsPerPage = 4;
                         }
                     });
                 }
