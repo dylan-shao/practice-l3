bf7bc894de8654545ab0f06372f4e04aa90f3b6d
shao <yangshao2014@gmail.com>
2016-04-03 11:23:04 -0400

    added modal using

--- public/app.js
+++ public/app.js
@@ -6,6 +6,14 @@
                     .then(function(data) {
                         $scope.interviews = data.data;
                     })
+                $scope.showQuestions = function(interview) {
+                    $uibModal.open({
+                        templateUrl: '/questions.html',
+                        controller: function($scope) {
+                            $scope.questions = interview.questions;
+                        }
+                    });
+                }
             }
         ])
 })();
